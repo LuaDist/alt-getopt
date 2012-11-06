c0e6a75c357eaefa9bc58426af3e9bb71228851b
Peter Drahoš <drahosp@gmail.com>
2012-11-06 14:06:46 +0100

    Updated to luarocks version of the package to avoid minor diffrerences between the packages

--- alt_getopt.lua
+++ alt_getopt.lua
@@ -21,6 +21,8 @@
 
 local type, pairs, ipairs, io, os = type, pairs, ipairs, io, os
 
+module ("alt_getopt")
+
 local function convert_short2long (opts)
    local i = 1
    local len = #opts
@@ -59,7 +61,7 @@ local function canonize (options, opt)
    return opt
 end
 
-local function get_ordered_opts (arg, sh_opts, long_opts)
+function get_ordered_opts (arg, sh_opts, long_opts)
    local i      = 1
    local count  = 1
    local opts   = {}
@@ -148,7 +150,7 @@ local function get_ordered_opts (arg, sh_opts, long_opts)
    return opts,i,optarg
 end
 
-local function get_opts (arg, sh_opts, long_opts)
+function get_opts (arg, sh_opts, long_opts)
    local ret = {}
 
    local opts,optind,optarg = get_ordered_opts (arg, sh_opts, long_opts)
@@ -162,8 +164,3 @@ local function get_opts (arg, sh_opts, long_opts)
 
    return ret,optind
 end
-
-return {
-    get_ordered_opts = get_ordered_opts,
-    get_opts = get_opts,
-}
